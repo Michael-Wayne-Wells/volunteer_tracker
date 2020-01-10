df18cc58a682302d057ad55661a1c5cf495f41e9
Michael Wells <mwells1286@gmail.com>
2020-01-10 13:00:02 -0800

    add ability to add and display volunteers.

--- app.rb
+++ app.rb
@@ -6,7 +6,6 @@ require('./lib/volunteer')
 require('pg')
 
 DB = PG.connect({:dbname => "volunteer_tracker"})
-
 also_reload('lib/**/*.rb')
 
 get("/") do
@@ -24,31 +23,63 @@ get("/projects") do
 end
 
 post("/projects") do
- title = params[:title]
- project = Project.new({:title => title, :id => nil})
- project.save
- redirect to("/projects")
+  if params[:title] =~ /^[a-z0-9\_\-]+\s?[a-z0-9\_\-]+$/i
+    project = Project.new({:title => title, :id => nil})
+    project.save
+    redirect to("/projects")
+  else
+    erb(:error)
+  end
 end
 
 get('/projects/:id') do
   @project = Project.find(params[:id].to_i)
+  @volunteers = @project.volunteers
   @show = params[:show]
   erb(:project)
 end
-get('/projects/:id/edit') do
+get("/projects/:id/volunteers/:volunteer_id") do
+
+end
+patch("/projects/:id/volunteers/:volunteer_id") do
+
+end
+
+post("/projects/:id/volunteers/") do
   @project = Project.find(params[:id].to_i)
+  vol_name = params[:vol_name]
+  if vol_name =~ /^[a-z0-9\_\-]+\s?[a-z0-9\_\-]+$/i
+    volunteer = (Volunteer.new({:name => vol_name, :project_id => @project.id, :id => nil}))
+    volunteer.save
+    @volunteers = @project.volunteers
+    erb(:project)
+  else
+    erb(:error)
+  end
+end
+
+delete("/projects/:id/volunteers/:volunteer_id") do
+
+end
+get('/projects/:id/edit') do
   @show = true
   erb(:project)
 end
 
 patch('/projects/:id') do
   @project = Project.find(params[:id].to_i)
-  @project.update({:title => params[:title], :id => nil})
-  @show = false
-  erb(:project)
+  if params[:title] =~ /^[a-z0-9\_\-]+\s?[a-z0-9\_\-]+$/i
+    @project.update({:title => params[:title], :id => nil})
+    @show = false
+    erb(:project)
+  else
+    erb(:error)
+  end
 end
+
 delete('/projects/:id') do
   project = Project.find(params[:id].to_i)
   project.delete
+  @projects = Project.all
   erb(:projects)
 end
