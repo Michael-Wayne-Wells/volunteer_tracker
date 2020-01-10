0daa2e36a07d79fb66dd168c8b84504e9fc3085a
Michael Wells <mwells1286@gmail.com>
2020-01-10 08:16:50 -0800

    add project files and folders

---
+++ app.rb
@@ -0,0 +1,10 @@
+require('sinatra')
+require('sinatra/reloader')
+require('pry')
+require('./lib/project')
+require('./lib/volunteer')
+require('pg')
+
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
+also_reload('lib/**/*.rb')
