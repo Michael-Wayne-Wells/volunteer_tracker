71fd1bc10fef673d82e05c6ae16b9a8a1e58a6b7
Michael Wells <mwells1286@gmail.com>
2020-01-10 10:02:51 -0800

    add save and all methods that pass rspec.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,16 +1,31 @@
 class Volunteer
-attr_accessor :name, :project_id
-attr_reader :id
+  attr_accessor :name, :project_id
+  attr_reader :id
 
-def initialize(attributes)
-  @name = attributes.fetch(:name)
-  @project_id = attributes.fetch(:project_id)
-  @id = attributes.fetch(:id)
-end
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
+  end
 
-def ==(other_volunteer)
-  (self.name == other_volunteer.name) && (self.id == other_volunteer.id)
-end
+  def ==(other_volunteer)
+    (self.name == other_volunteer.name) && (self.id == other_volunteer.id)
+  end
 
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) values ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = result.first.fetch("id").to_i
+  end
 
+  def self.all
+    results = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    results.each do |result|
+      name = result.fetch("name")
+      project_id = result.fetch("project_id").to_i
+      id = result.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
 end
