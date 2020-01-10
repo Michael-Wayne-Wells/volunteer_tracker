4165f2b34cce56e6c4dd901cdd150569b908e21a
Michael Wells <mwells1286@gmail.com>
2020-01-10 14:51:21 -0800

    add test for ui deleting a volunteer

--- spec/volunteer_integration_spec.rb
+++ spec/volunteer_integration_spec.rb
@@ -64,3 +64,16 @@ describe 'the volunteer detail page path', {:type => :feature} do
     expect(page).to have_content('Jane')
   end
 end
+describe 'the volunteer detail page path', {:type => :feature} do
+  it 'deletes a volunteer' do
+    test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+    test_project.save
+    project_id = test_project.id.to_i
+    test_volunteer = Volunteer.new({:name => 'Jasmine', :project_id => project_id, :id => nil})
+    test_volunteer.save
+    visit "/projects/#{project_id}"
+    click_link('Jasmine')
+    click_button('Delete')
+    expect(page).not_to have_content('Jasmine')
+  end
+end
